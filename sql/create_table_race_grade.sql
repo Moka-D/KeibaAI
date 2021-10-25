d16e82c3ba32ca910443ea1fb31988e06ae2b451
Masatoshi <flashback.3806_from.okayama@hotmail.co.jp>
2021-10-25 22:41:09 +0900

    sqlファイルをすべて削除

--- sql/create_table_race_grade.sql
+++
@@ -1,4 +0,0 @@
-create table race_grade (
-    grade_id SMALLINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    grade_name VARCHAR(10) UNIQUE
-);
--- sql/create_table_weather.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE weather (
-    weather_id SMALLINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    caption VARCHAR(2) UNIQUE
-);
