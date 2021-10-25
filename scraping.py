bc1534fef50ede0770110babb35f2ffdee4d5a97
Masatoshi <flashback.3806_from.okayama@hotmail.co.jp>
2021-10-26 00:29:53 +0900

    出馬表をスクレイピングする関数を追加

--- scraping.py
+++ scraping.py
@@ -8,16 +8,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
-class UnMatchExpectedData(Exception):
+def scrape_race_info(race_id: str) -> tuple[dict[str, str], pd.DataFrame, pd.DataFrame]:
     """
-    期待するデータが見つからなかったときの例外クラス
-    """
-    pass
-
-
-def scrape_race_info(race_id: str) -> tuple[pd.DataFrame, dict[str, str], pd.DataFrame]:
-    """
-    レース結果のスクレイピングを行う関数
+    レース結果をスクレイピングする関数
 
     Parameters
     ----------
@@ -26,10 +19,10 @@ def scrape_race_info(race_id: str) -> tuple[pd.DataFrame, dict[str, str], pd.Dat
 
     Returns
     -------
-    result_df : pandas.DataFrame
-        レース結果
     info_dict : dict[str, str]
         レース情報
+    result_df : pandas.DataFrame
+        出走馬一覧とレースの結果
     payoff_table : pandas.DataFrame
         払い戻し表
     """
@@ -50,7 +43,7 @@ def scrape_race_info(race_id: str) -> tuple[pd.DataFrame, dict[str, str], pd.Dat
     text1 = p_texts[0].text
     text2 = p_texts[1].text
 
-    # race_type
+    # race_type, turn
     if '障' in text1:
         info_dict['race_type'] = '障害'
     elif '芝' in text1:
@@ -58,14 +51,21 @@ def scrape_race_info(race_id: str) -> tuple[pd.DataFrame, dict[str, str], pd.Dat
     elif 'ダ' in text1:
         info_dict['race_type'] = 'ダート'
     else:
-        raise UnMatchExpectedData("期待するrace_typeが見つかりませんでした。")
+        info_dict['race_type'] = 'その他'
+
+    if '右' in text1:
+        info_dict['turn'] = '右'
+    elif '左' in text1:
+        info_dict['turn'] = '左'
+    else:
+        info_dict['turn'] = 'その他'
 
     # date
     info_dict['date'] = re.findall(r'\w+', text2)[0]
 
     # others info
     text_list = re.findall(r'\w+', text1)
-    for text in text_list:
+    for text in reversed(text_list):
         if 'm' in text:
             info_dict['course_dist'] = re.findall(r'\d+', text)[0]
         if text in ['良', '稍重', '重', '不良']:
@@ -105,12 +105,12 @@ def scrape_race_info(race_id: str) -> tuple[pd.DataFrame, dict[str, str], pd.Dat
     dfs = pd.read_html(html_p)
     payoff_table = pd.concat([dfs[1], dfs[2]], ignore_index=True)
 
-    return result_df, info_dict, payoff_table
+    return info_dict, result_df, payoff_table
 
 
 def scrape_horse_peds(horse_id: str) -> pd.DataFrame:
     """
-    馬の血統(2世代前まで)をスクレイピングで取得する関数
+    馬の血統(2世代前まで)をスクレイピングする関数
 
     Parameters
     ----------
@@ -134,3 +134,88 @@ def scrape_horse_peds(horse_id: str) -> pd.DataFrame:
 
     peds_df = pd.concat([generations[i] for i in range(columns_num)], ignore_index=True).rename(horse_id)
     return peds_df
+
+
+def scrape_race_card(race_id: str, date: str) -> pd.DataFrame:
+    """
+    出馬表をスクレイピングする関数
+
+    Parameters
+    ----------
+    race_id : str
+        レースID
+    date : str
+        レースの日付
+
+    Returns
+    -------
+    race_card_df : pd.DataFrame
+        出馬表
+    """
+    url = 'https://race.netkeiba.com/race/shutuba.html?race_id=' + race_id
+    df = pd.read_html(url)[0]
+    df = df.T.reset_index(level=0, drop=True).T
+
+    html = requests.get(url)
+    html.encoding = 'EUC-JP'
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    # レース情報
+    info_texts = soup.find('div', attrs={'class': 'RaceData01'}).text
+    info = re.findall(r'\w+', info_texts)
+
+    if '障' in info_texts:
+        df['race_type'] = ['障害'] * len(df)
+    elif '芝' in info_texts:
+        df['race_type'] = ['芝'] * len(df)
+    elif 'ダ' in info_texts:
+        df['race_type'] = ['ダート'] * len(df)
+
+    if '右' in info_texts:
+        df['turn'] = ['右'] * len(df)
+    elif '左' in info_texts:
+        df['turn'] = ['左'] * len(df)
+    else:
+        df['turn'] = ['その他'] * len(df)
+
+    for text in info:
+        if 'm' in text:
+            df['course_dist'] = [int(re.findall(r'\d+', text)[0])] * len(df)
+        if text in ['良', '重']:
+            df['ground_state'] = [text] * len(df)
+        if text in ['不']:
+            df['ground_state'] = ['不良'] * len(df)
+        if text in ['稍']:
+            df['ground_state'] = ['稍重'] * len(df)
+        if text in ['曇', '晴', '雨', '小雨', '小雪', '雪']:
+            df['weather'] = [text] * len(df)
+
+    df['date'] = [date] * len(df)
+
+    # horse_id
+    horse_id_list = []
+    horse_td_list = soup.find_all('td', attrs={'class': 'HorseInfo'})
+    for td in horse_td_list:
+        horse_id = re.findall(r'\d+', td.find('a')['href'])
+        horse_id_list.append(horse_id[0])
+
+    # jockey_id
+    jockey_id_list = []
+    jockey_td_list = soup.find_all('td', attrs={'class': 'Jockey'})
+    for td in jockey_td_list:
+        jockey_id = re.findall(r'\d+', td.find('a')['href'])
+        jockey_id_list.append(jockey_id[0])
+
+    # trainer_id
+    trainer_id_list = []
+    trainer_td_list = soup.find_all('td', attrs={'class': 'Trainer'})
+    for td in trainer_td_list:
+        trainer_id = re.findall(r'\d+', td.find('a')['href'])
+        trainer_id_list.append(trainer_id[0])
+
+    df['horse_id'] = horse_id_list
+    df['jockey_id'] = jockey_id_list
+    df['trainer_id'] = trainer_id_list
+
+    race_card_df = df.drop(['印', 'Unnamed: 9_level_1', '人気', '登録', 'メモ'], axis=1)
+    return race_card_df
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
+import datetime
 
 
 MAX_PLACE_NUM = 11
@@ -9,14 +10,37 @@ MAX_DAY_NUM = 13
 MAX_RACE_NUM = 13
 
 
-def create_race_id_list(start_year, last_year):
+def create_race_id_list(start_year: int, last_year: int) -> list[str]:
+    """
+    指定した年範囲のレースIDを生成する関数
+
+    Parameters
+    ----------
+    start_year : int
+        開始年 (>= 1975)
+    last_year : int
+        終了年 (<= 今年 and >= start_year)
+
+    Returns
+    -------
+    race_id_list : list[str]
+        レースIDのリスト
+    """
+    # 引数チェック
+    if start_year > last_year:
+        raise ValueError("start_year は last_year 以下の値を設定してください。")
+    if start_year < 1975:
+        raise ValueError("start_year は 1975 以上の整数を指定してください。")
+    if last_year > datetime.date.today().year:
+        raise ValueError("last_year は %d 以下の整数を指定してください。" % datetime.date.today().year)
+
     race_id_list = []
-    if start_year <= last_year and start_year >= 1975 and last_year <= datetime.date.today().year:
-        for year in range(start_year, last_year + 1):
-            for place in range(1, MAX_PLACE_NUM):
-                for hold in range(1,MAX_HOLD_NUM):
-                    for day in range(1, MAX_DAY_NUM):
-                        for race in range(1, MAX_RACE_NUM):
-                            race_id = "{:4d}{:0>2d}{:0>2d}{:0>2d}{:0>2d}".format(year, place, hold, day, race)
-                            race_id_list.append(race_id)
+    for year in range(start_year, last_year + 1):
+        for place in range(1, MAX_PLACE_NUM):
+            for hold in range(1,MAX_HOLD_NUM):
+                for day in range(1, MAX_DAY_NUM):
+                    for race in range(1, MAX_RACE_NUM):
+                        race_id = "{:4d}{:0>2d}{:0>2d}{:0>2d}{:0>2d}".format(year, place, hold, day, race)
+                        race_id_list.append(race_id)
+
     return race_id_list
